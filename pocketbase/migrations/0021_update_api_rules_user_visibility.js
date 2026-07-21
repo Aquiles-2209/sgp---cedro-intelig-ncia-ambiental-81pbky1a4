17f9487bdd304d8cb737268cf207facda46be8cf
Skip <skip@usecurling.com>
2026-07-21 14:39:35 -0300

    Refactor project access for user role

--- pocketbase/migrations/0021_update_api_rules_user_visibility.js
+++ pocketbase/migrations/0021_update_api_rules_user_visibility.js
@@ -2,16 +2,16 @@ migrate(
   (app) => {
     const projectsCol = app.findCollectionByNameOrId('projects')
     projectsCol.listRule =
-      "@request.auth.role = 'admin' || id = @collection.allocations.project ? user = @request.auth.id"
+      "@request.auth.role = 'admin' || (@collection.allocations.project ?= id && @collection.allocations.user ?= @request.auth.id)"
     projectsCol.viewRule =
-      "@request.auth.role = 'admin' || id = @collection.allocations.project ? user = @request.auth.id"
+      "@request.auth.role = 'admin' || (@collection.allocations.project ?= id && @collection.allocations.user ?= @request.auth.id)"
     app.save(projectsCol)
 
     const tasksCol = app.findCollectionByNameOrId('tasks')
     tasksCol.listRule =
-      "@request.auth.role = 'admin' || project = @collection.allocations.project ? user = @request.auth.id"
+      "@request.auth.role = 'admin' || (@collection.allocations.project ?= project && @collection.allocations.user ?= @request.auth.id)"
     tasksCol.viewRule =
-      "@request.auth.role = 'admin' || project = @collection.allocations.project ? user = @request.auth.id"
+      "@request.auth.role = 'admin' || (@collection.allocations.project ?= project && @collection.allocations.user ?= @request.auth.id)"
     app.save(tasksCol)
 
     const allocationsCol = app.findCollectionByNameOrId('allocations')
